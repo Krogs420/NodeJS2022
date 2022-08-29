dabfe82179ee339611569a58fe6ec8ab285228fd
Kristian Kryger <kristianlaukryger@gmail.com>
2022-08-29 10:02:49 +0200

    Added the type coersion file and intro exercieses basic files og flyttet mappe

---
+++ 0._Miscellaneous/type_coersion.js
@@ -0,0 +1,10 @@
+//Type coersion lecture
+const total = 34.32;
+const price = 12.32;
+
+const newTotal = price + total;
+
+console.log(newTotal);
+
+const balanceEquals = total === newTotal;
+console.log(balanceEquals);
